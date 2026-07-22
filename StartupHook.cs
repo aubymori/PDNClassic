c86823218bb5bd5d976fccd02c042476f85f986b
aubymori <aubyomori@gmail.com>
2026-07-22 17:48:03 -0500

    Initial commit

---
+++ StartupHook.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using PaintDotNet.DirectWrite;
+using PaintDotNet.Rendering;
+using System;
+using System.Windows;
+
+internal class StartupHook
+{
+    private static Harmony harmony = new Harmony("aubymori.pdnclassic");
+
+    public static void Initialize()
+    {
+        try
+        {
+            harmony = new Harmony("aubymori.pdnclassic");
+
+
+        }
+        catch (Exception e)
+        {
+            System.Windows.MessageBox.Show($"{e.Message}\n{e.StackTrace}", "PDNClassic", MessageBoxButton.OK, MessageBoxImage.Error);
+            Environment.Exit(0);
+        }
+    }
+}
